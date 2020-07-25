4f836f59c5343326fce92924a8e189646aa29a22
Hikmat Jafarli <jafarlihi@gmail.com>
2020-07-26 00:26:13 +0400

    Add handler tests

---
+++ handlers/contact_test.go
@@ -0,0 +1,30 @@
+package handlers_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/jafarlihi/addressbook/handlers"
+)
+
+func TestCreateContactNoBody(t *testing.T) {
+	req, err := http.NewRequest("POST", "/api/contact", strings.NewReader(""))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.CreateContact)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+
+	expected := `{"error": "Request body couldn't be parsed as JSON"}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
