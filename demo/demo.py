9871e2bd75690ebe0fe258efba4e1fa9f5b56f76
Hikmat Jafarli <jafarlihi@gmail.com>
2020-07-26 13:20:24 +0400

    Pretty print JSON responses in demo script

--- demo/demo.py
+++ demo/demo.py
@@ -14,14 +14,16 @@ if __name__ == "__main__":
                'password': password, 'email': 'some@email.com'}
     print('POSTing to /api/user with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/user', json=payload)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Creating a token')
     payload = {'username': username, 'password': password}
     print('POSTing to /api/user/token with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/user/token', json=payload)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     token = "Bearer " + json.loads(response.content)["token"]
     print("Extracted token and using it in Authorization header for all subsequent requests: " + token)
     headers = {'Authorization': token}
@@ -32,7 +34,8 @@ if __name__ == "__main__":
     print('POSTing to /api/contact with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Creating another contact')
@@ -41,7 +44,8 @@ if __name__ == "__main__":
     print('POSTing to /api/contact with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     contactID1 = json.loads(response.content)["id"]
     print('')
 
@@ -51,44 +55,49 @@ if __name__ == "__main__":
     print('POSTing to /api/contact with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     contactID2 = json.loads(response.content)["id"]
     print('')
 
     print('Creating another contact')
-    payload = {'name': 'name3', 'surname': 'surname3',
-               'email': 'valid3@mail.com'}
+    payload = {'name': 'name4', 'surname': 'surname4',
+               'email': 'valid4@mail.com'}
     print('POSTing to /api/contact with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     contactID3 = json.loads(response.content)["id"]
     print('')
 
     print('Fetching all contacts')
     print('GETing /api/contact')
     response = requests.get(address + '/api/contact', headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Fetching single contact with ID of ' + str(contactID3))
     print('GETing /api/contact/' + str(contactID3))
     response = requests.get(address + '/api/contact/' +
                             str(contactID3), headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Deleting a contact with ID of ' + str(contactID3))
     print('DELETEing /api/contact/' + str(contactID3))
     response = requests.delete(address + '/api/contact/' +
                                str(contactID3), headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' + response.text)
     print('')
 
     print('Fetching all contacts')
     print('GETing /api/contact')
     response = requests.get(address + '/api/contact', headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Creating a contact-list')
@@ -96,7 +105,8 @@ if __name__ == "__main__":
     print('POSTing to /api/contact-list with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     contactListID1 = json.loads(response.content)["id"]
     print('')
 
@@ -105,7 +115,8 @@ if __name__ == "__main__":
     print('POSTing to /api/contact-list with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     contactListID2 = json.loads(response.content)["id"]
     print('')
 
@@ -114,34 +125,38 @@ if __name__ == "__main__":
     print('POSTing to /api/contact-list with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     contactListID3 = json.loads(response.content)["id"]
     print('')
 
     print('Fetching all contact-lists')
     print('GETing /api/contact-list')
     response = requests.get(address + '/api/contact-list', headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Fetching single contact-list with ID of ' + str(contactListID3))
     print('GETing /api/contact-list/' + str(contactListID3))
     response = requests.get(address + '/api/contact-list/' +
                             str(contactListID3), headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Deleting a contact-list with ID of ' + str(contactListID3))
     print('DELETEing /api/contact-list/' + str(contactListID3))
     response = requests.delete(address + '/api/contact-list/' +
                                str(contactListID3), headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' + response.text)
     print('')
 
     print('Fetching all contact-lists')
     print('GETing /api/contact-list')
     response = requests.get(address + '/api/contact-list', headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Creating another contact-list')
@@ -149,7 +164,8 @@ if __name__ == "__main__":
     print('POSTing to /api/contact-list with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     contactListID4 = json.loads(response.content)["id"]
     print('')
 
@@ -158,7 +174,8 @@ if __name__ == "__main__":
     print('POSTing to /api/contact-list/search with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list/search',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Conducting another search on contact-lists')
@@ -166,7 +183,8 @@ if __name__ == "__main__":
     print('POSTing to /api/contact-list/search with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list/search',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Adding contact ID ' + str(contactID1) +
@@ -176,7 +194,7 @@ if __name__ == "__main__":
           '/contact with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list/'+str(contactListID1)+'/contact',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' + response.text)
     print('')
 
     print('Adding contact ID ' + str(contactID2) +
@@ -186,14 +204,15 @@ if __name__ == "__main__":
           '/contact with payload: ' + json.dumps(payload))
     response = requests.post(address + '/api/contact-list/'+str(contactListID1)+'/contact',
                              json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' + response.text)
     print('')
 
     print('Listing contacts of contact-list ID ' + str(contactListID1))
     print('GETing to /api/contact-list/' + str(contactListID1) + '/contact')
     response = requests.get(address + '/api/contact-list/' +
                             str(contactListID1) + '/contact', headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
     print('')
 
     print('Deleting contact ID ' + str(contactID2) +
@@ -203,11 +222,12 @@ if __name__ == "__main__":
           '/contact with payload: ' + json.dumps(payload))
     response = requests.delete(address + '/api/contact-list/'+str(contactListID1)+'/contact',
                                json=payload, headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' + response.text)
     print('')
 
     print('Listing contacts of contact-list ID ' + str(contactListID1))
     print('GETing to /api/contact-list/' + str(contactListID1) + '/contact')
     response = requests.get(address + '/api/contact-list/' +
                             str(contactListID1) + '/contact', headers=headers)
-    print('Received reply: ' + str(response.content))
+    print('Received reply: ' +
+          json.dumps(json.loads(response.text), indent=4, sort_keys=True))
